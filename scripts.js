957192992865e38162bb7e5a09af3d90e3187b49
panane1 <156020514+panane1@users.noreply.github.com>
2024-03-04 17:02:07 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,22 +7,5 @@ function validateForm() {
       alert('Please fill out all fields.');
       return false;
   }
-
-  // Additional validation logic can be added here
-
-  // If all validation passes, show success message
-  showSuccessMessage();
-  return true;
-}
-
-function showSuccessMessage() {
-  var form = document.getElementById('contactForm');
-  var successMessage = document.getElementById('successMessage');
-
-  // Hide the form
-  form.style.display = 'none';
-
-  // Show the success message
-  successMessage.classList.remove('hidden');
-}
+};
 
