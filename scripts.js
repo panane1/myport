40be0b8c1cda5d5fa3677c6cb4e3019336b675a5
panane1 <156020514+panane1@users.noreply.github.com>
2024-03-04 17:27:11 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-function validateForm() {
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  var message = document.getElementById('message').value;
-
-  if (!name || !email || !message) {
-      alert('Please fill out all fields.');
-      return false;
-  }
-};
 
